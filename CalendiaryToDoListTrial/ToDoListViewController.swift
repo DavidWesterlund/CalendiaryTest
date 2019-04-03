375ccd714cd25707381813ff6433a1a346983a92
DavidWesterlund <david.westerlund@gmail.com>
2019-04-03 16:21:28 +1100

    Add new item functionality complete

--- CalendiaryToDoListTrial/ToDoListViewController.swift
+++ CalendiaryToDoListTrial/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["List 1", "List 2", "List 3"]
+    var itemArray = ["List 1", "List 2", "List 3"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +40,32 @@ class ToDoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
+    
+    //MARK - Add new items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Calendo Item", message: "", preferredStyle: .alert)
+     
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //What will happen once the user has pressed the ADD ITEM alert on UIAlert
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
